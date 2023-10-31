63b243335be6765c7f1de1bb51196ad983938d26
nono <nour98nour@gmail.com>
2023-10-30 17:23:36 -0700

    10 task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,14 +6,17 @@
  *
  * Return: true or false
  */
- 
 int check_cycle(listint_t *list)
 {
-	listint_t *curr = list->next;
+	listint_t *curr;
+
+	if (!list)
+		return (0);
+	curr = list;
 	while (curr->next != NULL && curr->next != list)
 		curr = curr->next;
 	if (curr->next == NULL)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 }
