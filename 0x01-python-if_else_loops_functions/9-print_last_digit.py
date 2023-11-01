0bf7d838966c46526e10d4fd601228cc90db7a7b
nono <nour98nour@gmail.com>
2023-10-31 18:01:15 -0700

    9 task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(number % 10, end="")
-    return number % 10
+    if number < 0:
+        mod = -number % 10
+    else:
+        mod = number % 10
+    print(mod, end="")
+    return mod
