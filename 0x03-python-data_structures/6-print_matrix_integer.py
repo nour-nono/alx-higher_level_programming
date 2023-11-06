c74783eb28bba9e2de40442e45ea4a59d08c1f40
nono <nour98nour@gmail.com>
2023-11-06 15:49:11 -0800

    6 task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
+    if len(matrix) == 0:
+        print()
+    else:
+        for arr in (matrix):
+            for ele in range(0, len(arr)):
+                print("{:d}".format(arr[ele]), end="")
+                if ele != len(arr)-1:
+                    print(end=" ")
+            print()
