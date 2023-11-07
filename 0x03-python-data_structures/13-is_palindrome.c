d19f8791e57094cf1b64d89442d25c3b77adf986
nono <nour98nour@gmail.com>
2023-11-06 17:43:09 -0800

    13 task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: True or False
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	listint_t **head_copy = head;
+	return (recursive_palindrom(head_copy, *head));
+}
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @end: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+recursive_palindrom(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (recursive_palindrom(head, end->next) && ((*head)->n == end->n))
+	{
+		(*head) = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
