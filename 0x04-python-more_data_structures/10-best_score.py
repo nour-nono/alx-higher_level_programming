88580139a0b0f0f44b70089af0ddd57d02748e3f
nono <nour98nour@gmail.com>
2023-11-08 15:58:47 -0800

    10 task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return
+        return None
     a = max(a_dictionary.values())
     for x in a_dictionary.keys():
         if a == a_dictionary[x]:
