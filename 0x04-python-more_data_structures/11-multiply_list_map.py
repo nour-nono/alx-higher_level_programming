6f2d4b53f5d89d1b4366d4992188bc71cf957f97
nono <nour98nour@gmail.com>
2023-11-08 16:26:44 -0800

    11 task

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,7 +2,5 @@
 def multiply_list_map(my_list=[], number=0):
     if my_list is None:
         return
-    if number is None:
-        return my_list
     a = list(map(lambda x: x * number, my_list))
     return a
