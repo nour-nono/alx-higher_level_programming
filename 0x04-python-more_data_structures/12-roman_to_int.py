3ef641cc8970001500dda223b65fd19f3481d07b
nono <nour98nour@gmail.com>
2023-11-08 16:52:02 -0800

    12 task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+        a_dictionary = {'M': 1000, 'D': 500, 'C': 100,
+                        'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    add, sub = a_dictionary[roman_string[-1]], 0
+    for x in range(len(roman_string) - 1):
+        if a_dictionary[roman_string[x]] < a_dictionary[roman_string[x + 1]]:
+            sub += a_dictionary[roman_string[x]]
+        else:
+            add += a_dictionary[roman_string[x]]
+    return add - sub
