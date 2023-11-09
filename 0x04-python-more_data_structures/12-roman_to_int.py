495f4356d16d8cdbca0a777a919a7ced92e140ed
nono <nour98nour@gmail.com>
2023-11-08 17:04:32 -0800

    12 task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,7 @@ def roman_to_int(roman_string):
         return 0
         a_dictionary = {'M': 1000, 'D': 500, 'C': 100,
                         'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    roman_string = roman_string.strip().upper()
     add, sub = a_dictionary[roman_string[-1]], 0
     for x in range(len(roman_string) - 1):
         if a_dictionary[roman_string[x]] < a_dictionary[roman_string[x + 1]]:
