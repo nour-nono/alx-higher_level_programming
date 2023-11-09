447b7779babcad5f95e12e23e132abf59028e4b0
nono <nour98nour@gmail.com>
2023-11-08 17:25:14 -0800

    12 task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,22 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
-        a_dictionary = {'M': 1000, 'D': 500, 'C': 100,
-                        'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    roman_string = roman_string.strip().upper()
-    add, sub = a_dictionary[roman_string[-1]], 0
-    for x in range(len(roman_string) - 1):
-        if a_dictionary[roman_string[x]] < a_dictionary[roman_string[x + 1]]:
-            sub += a_dictionary[roman_string[x]]
+
+    a_dictionary = {'M': 1000,
+                    'D': 500,
+                    'C': 100,
+                    'L': 50,
+                    'X': 10,
+                    'V': 5,
+                    'I': 1}
+    roman_string2 = []
+    for x in range(len(roman_string)):
+        if roman_string[x] in a_dictionary.keys():
+            roman_string2.append(roman_string[x])
+    add, sub = a_dictionary[roman_string2[-1]], 0
+    for x in range(len(roman_string2) - 1):
+        if a_dictionary[roman_string2[x]] < a_dictionary[roman_string2[x + 1]]:
+            sub += a_dictionary[roman_string2[x]]
         else:
-            add += a_dictionary[roman_string[x]]
+            add += a_dictionary[roman_string2[x]]
     return add - sub
