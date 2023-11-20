73776455974c62dcada0aa14fea59ef84e2b3e30
nono <nour98nour@gmail.com>
2023-11-20 14:19:31 -0800

    init

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,6 @@
 def safe_print_list(my_list=[], x=0):
     try:
         z = 0
-        if type(my_list) is int:
-            print(my_list)
-            return 1
         for y in my_list:
             if z >= x:
                 print()
@@ -13,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             z += 1
         print()
         return z
-    except TypeError:
+    except:
         return 0
