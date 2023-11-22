22e26a7d9d36b0932f848ac47b46e7b344e39b07
nono <nour98nour@gmail.com>
2023-11-21 18:15:06 -0800

    6 task

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" this is square module, and it is the first in oop """
+
+
+class Square:
+    """ this is square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """ this is the constructor
+            Args:
+                size: The first parameter.
+                position: square position in x-axis and y-axis
+            Raises:
+                TypeError: If size is not an integer or position is not a tuple of 2 positive integers.
+                ValueError: If size is less than 0.
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """methode area to compute the area of the square
+
+           Returns:
+               the area of the square.
+           """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ property size is the size of each side of the square
+
+           Returns:
+               the size of the square.
+           """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ methode size is a setter for size attribute
+            Args:
+                value: size value.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ property size is the size of each side of the square
+
+           Returns:
+               the size of the square.
+           """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ methode size is a setter for size attribute
+            Args:
+                value: size value.
+        """
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """ methode my_print prints in stdout the square with the character #
+        """
+        for x in range(self.__size):
+            if self.__position[1] <= 0:
+                for y in range(self.__position[0]):
+                    print(" ", end="")
+            for y in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
