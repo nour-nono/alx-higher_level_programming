ffc262494bb21f6d248f602475a1fb0845c98048
nono <nour98nour@gmail.com>
2023-11-21 16:40:10 -0800

    1 task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this is square module, and it is the first in oop """
+
+
+class Square:
+    """ this is square class"""
+    def __init__(self, size):
+        """ this is the constructor
+            Args:
+                size: The first parameter.
+        """
+        self.__size = size
