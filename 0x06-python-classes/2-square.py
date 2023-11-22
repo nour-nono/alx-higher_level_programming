c25e593da1718ca477f84073f95f53aeeefeca23
nono <nour98nour@gmail.com>
2023-11-21 17:11:58 -0800

    2 task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this is square module, and it is the first in oop """
+
+
+class Square:
+    """ this is square class"""
+    def __init__(self, size=0):
+        """ this is the constructor
+            Args:
+                size: The first parameter.
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
