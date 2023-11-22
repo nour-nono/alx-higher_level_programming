3e84d2ee38179c1d312ecda8aa19eaf894c41623
nono <nour98nour@gmail.com>
2023-11-21 18:26:04 -0800

    6 task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,7 +10,8 @@ class Square:
                 size: The first parameter.
                 position: square position in x-axis and y-axis
             Raises:
-                TypeError: If size is not an integer or position is not a tuple of 2 positive integers.
+                TypeError: If size is not an integer
+                or position is not a tuple of 2 positive integers.
                 ValueError: If size is less than 0.
         """
         self.__size = size
@@ -60,7 +61,9 @@ class Square:
             Args:
                 value: size value.
         """
-        if type(value) is not tuple or len(value) != 2:
+        if type(value) is not tuple or len(value) != 2\
+            or type(value[0]) is not int or type(value) is not int\
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
