f8b690c64ed44bb5f2227e4c87cfc3279c9d9cb3
nono <nour98nour@gmail.com>
2023-12-04 15:30:28 -0800

    init

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This function returns a list of object attributes.
+"""
+
+
+def lookup(obj):
+    """
+    This function returns a list of object attributes.
+
+    Parameters:
+        obj (object): The object to inspect.
+    
+    Returns:
+        list: A list of the object's attributes.
+
+    """
+    return dir(obj)
