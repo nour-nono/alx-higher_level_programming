e687df0c3420dec3a464bfba939814d337ee9ad0
nono <nour98nour@gmail.com>
2023-12-04 15:46:05 -0800

    1 task

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a class `MyList`.
+"""
+
+
+class MyList(list):
+    """
+    A class that extends the built-in Python `list` class.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the elements of the list in ascending order.
+
+        This method does not return anything. It directly prints the sorted
+        elements of the list.
+        """
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list")
