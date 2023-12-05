1f49d57e90c20fe1036519bbd7fc4c20079c0e0a
nono <nour98nour@gmail.com>
2023-12-04 16:47:13 -0800

    6 task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a class `MyList`.
+"""
+
+
+class BaseGeometry:
+    """
+    This module defines a class `MyList`.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
