916d5a8f8ee4b6e35a76b5d221b4d8f5b28eceb4
nono <nour98nour@gmail.com>
2023-12-04 16:38:20 -0800

    4 task

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a class `MyList`.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if an object is an instance of a given class.
+    Parameters:
+        obj (object): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+    bool: `True` or `False`.
+    """
+    return issubclass(obj, a_class)
