d5d5d0461ab3a354f113edac374865b9c4eccaec
nono <nour98nour@gmail.com>
2023-12-05 11:14:52 -0800

    init

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides a function for read a text from file.
+"""
+
 
 def read_file(filename=""):
+    """
+    Reads the content of a specified file and prints it.
+
+    Parameters:
+        filename (str): The name of the file to read from.
+
+    Returns:
+        None
+    """
     with open(filename, "r") as fl:
         print(fl.read())
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+This module provides a function for writing text to a file.
+"""
+
 
 def write_file(filename="", text=""):
+    """
+    Writes a given text to a specified file and returns the length of the text.
+
+    Parameters:
+        filename (str): The name of the file to write to.
+        text (str): The text to be written to the file.
+
+    Returns:
+        int: The length of the written text.
+    """
     with open(filename, "w") as fl:
         fl.write(text)
         return len(text)
