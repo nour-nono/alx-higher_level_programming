3f4bf11f4b7880a0342b358e21b40a2de98b116b
nono <nour98nour@gmail.com>
2023-12-04 16:35:02 -0800

    3 task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class `MyList`.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if an object is an instance of a given class.
+
+    Parameters:
+        obj (object): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+    bool: `True` `False` .
+    """
+    return issubclass(obj, a_class)
