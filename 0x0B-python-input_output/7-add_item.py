a3e26741b790b003d1632922f01fe0b83e75bee8
nono <nour98nour@gmail.com>
2023-12-05 15:30:50 -0800

    7 task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,13 @@ This script manages a list of items stored in a JSON file.
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 ls = []
+with open("add_item.json", "a") as fl:
+    pass
 try:
-    ls.extend(list(load_from_json_file("add_item.json")))
+    ls.extend(load_from_json_file("add_item.json"))
 except Exception:
     pass
 
-    ls.extend(sys.argv[1:])
-    save_to_json_file(ls, "add_item.json")
+ls.extend(sys.argv[1:])
+save_to_json_file(ls, "add_item.json")
