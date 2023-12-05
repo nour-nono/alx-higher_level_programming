685e470cc766545ea50955d7136c998d21660af3
nono <nour98nour@gmail.com>
2023-12-04 16:41:35 -0800

    4 task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,6 @@ def inherits_from(obj, a_class):
         a_class (type): The class to check against.
 
     Returns:
-    bool: `True` or `False`.
+        bool: `True` or `False`.
     """
-    return issubclass(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) == a_class
