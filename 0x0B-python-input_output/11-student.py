34c3f6eeda39abe18dd788d4bc5b4e263c8e5a9f
nono <nour98nour@gmail.com>
2023-12-05 16:46:05 -0800

    11 task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This module provide a class Student.
+"""
+
+
+class Student:
+    """
+    A class representing a student with attr: first_name, last_name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student object.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Convert the Student object's attributes into a dictionary.
+        Args:
+            attrs (list): attr list.
+
+        Returns:
+            dict: A dictionary representation of the Student object's attr.
+        """
+        if not isinstance(attrs, list):
+            return self.__dict__
+        ans = {}
+        for x in attrs:
+            if not isinstance(x, str):
+                return self.__dict__
+            else:
+                try:
+                    ans[x] = self.__getattribute__(x)
+                except AttributeError:
+                    pass
+        return ans
+
+    def reload_from_json(self, json):
+        """
+        Reload the Student object from a dictionary.
+
+        Args:
+            json (dict): A dictionary to reload the Student object from.
+        """
+        for x in json:
+            try:
+                self.x = json[x]
+            except Exception:
+                pass
