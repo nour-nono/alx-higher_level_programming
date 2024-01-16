3f9fb8f28acf9c3ccca478e68e267a7aa7427424
nono <nour98nour@gmail.com>
2024-01-16 15:45:18 -0800

    14 SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -4,6 +4,6 @@
 -- Records should be listed by descending score
 -- The database name will be passed as an argument of the mysql command
 SELECT score, name 
-FROM hbtn_0c_0.second_table 
+FROM second_table 
 WHERE name IS NOT NULL 
 ORDER BY score DESC;
