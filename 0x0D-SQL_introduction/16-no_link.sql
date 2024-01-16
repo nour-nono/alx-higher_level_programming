ee11c3229893c1852d5e4140e06d82efb730ebdd
nono <nour98nour@gmail.com>
2024-01-16 15:44:31 -0800

    14 SQL

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name 
+FROM hbtn_0c_0.second_table 
+WHERE name IS NOT NULL 
+ORDER BY score DESC;
