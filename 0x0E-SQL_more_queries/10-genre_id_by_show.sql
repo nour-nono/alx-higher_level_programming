10d461470447570f7a35b1a9b449162334ab10af
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-01-18 01:07:39 +0200

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,5 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tvshow_id
-GROUP BY tv_shows.title, tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tvshow_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
