34c0328728a597d1ae5cb0c9ad1070a2e4cdf7af
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-01-18 01:12:07 +0200

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,8 +5,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT 
-   tv_shows.title AS 'Title', 
-   tv_show_genres.genre_id AS 'Genre ID'
+   tv_shows.title, 
+   tv_show_genres.genre_id
 FROM 
    tv_shows 
 LEFT JOIN 
