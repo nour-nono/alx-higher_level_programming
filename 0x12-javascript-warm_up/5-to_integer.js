b44ff8aae597b8b55e6d37d89936365676ece26d
nono <nour98nour@gmail.com>
2024-02-12 19:59:58 +0200

    fix linting 5 task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const x = +process.argv[2]? `My number: ${+process.argv[2]}` : 'Not a number';
+const x = +process.argv[2] ? `My number: ${+process.argv[2]}` : 'Not a number';
 console.log(x);
