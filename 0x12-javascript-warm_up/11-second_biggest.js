c2ec79a06602c1ba755d0dbecb864c5800d45eec
nono <nour98nour@gmail.com>
2024-02-13 03:20:41 +0200

    11 task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-const arr = process.argv.slice(2);
-for (let index = 0; index < arr.length; index++) {
-  arr[index] = +arr[index];
+if (process.argv.length < 4) {
+  console.log("0");
+} else {
+  const arr = process.argv.slice(2);
+  for (let index = 0; index < arr.length; index++) {
+    arr[index] = +arr[index];
+  }
+  arr.sort().reverse();
+  console.log(arr[1] || 0);
 }
-arr.sort().reverse();
-console.log(arr[1] || 0);
