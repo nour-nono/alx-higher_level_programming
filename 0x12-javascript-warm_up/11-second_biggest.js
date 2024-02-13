d99d6778071a93cda5fcf5ba4e5864d54736cdca
nono <nour98nour@gmail.com>
2024-02-13 03:28:40 +0200

    11 again

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ const arr = process.argv.slice(2);
 for (let index = 0; index < arr.length; index++) {
   arr[index] = +arr[index];
 }
-arr.sort((a, b) => (b - a));
+arr.sort((a, b) => b - a);
 console.log(arr[1] || 0);
