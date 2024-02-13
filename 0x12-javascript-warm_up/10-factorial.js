b11dfb4f4d9f7f8aef52409871702d682a3c5a14
nono <nour98nour@gmail.com>
2024-02-13 03:07:09 +0200

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-function factorial(a) {
-    if (isNaN(a)) {
-        return 1;
-    }
-    if (a<1) {
-        return 0;
-    }
-    if (a === 1) {
-        return a;
-    }
-    return a*factorial(a-1);
+function factorial (a) {
+  if (isNaN(a)) {
+    return 1;
+  }
+  if (a < 1) {
+    return 0;
+  }
+  if (a === 1) {
+    return a;
+  }
+  return a * factorial(a - 1);
 }
 console.log(factorial(+process.argv[2]));
