f9b953cdbf1be495477a35324770a988a798daaa
nono <nour98nour@gmail.com>
2024-02-13 02:06:11 +0200

    10 task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+function factorial(a) {
+    if (a<1) {
+        return 1;
+    }
+    if (a === 1) {
+        return a;
+    }
+    return a*factorial(a-1);
+}
+console.log(factorial(process.argv[2]));
