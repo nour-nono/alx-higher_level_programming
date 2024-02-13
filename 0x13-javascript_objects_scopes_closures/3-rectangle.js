b2c0f9eb6d1cdef034e2167ca0294f13d2bb344f
nono <nour98nour@gmail.com>
2024-02-13 18:28:02 +0200

    3 task

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    this.print = function () {
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
+          process.stdout.write("X");
+        }
+        console.log();
+      }
+    };
+  }
+}
