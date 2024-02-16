9b601fab48072754429dc3214444760ae5644842
nono <nour98nour@gmail.com>
2024-02-16 15:21:24 +0200

    0 task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,13 @@
+"""
+This script connects to a MySQL database and fetches all rows from
+the 'states' table,ordered by the 'id' column. The script expects
+the database credentials as command-line
+arguments: the username, password, and database name.
+"""
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import MySQLdb, sys
+    import MySQLdb
+    import sys
 
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
