9778eb8faa7d9467c6abef0a3539e3857cf942d1
nono <nour98nour@gmail.com>
2024-02-16 17:00:22 +0200

    4 task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT `state_id`, `cities`.`name`, `states`.`name`"
+    cur.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`"
                 " FROM cities"
-                " LEFT JOIN states ON `cities`.`state_id` = `states`.`id`"
+                " JOIN states ON `cities`.`state_id` = `states`.`id`"
                 " ORDER BY `cities`.`id`")
     rows = cur.fetchall()
     for row in rows:
