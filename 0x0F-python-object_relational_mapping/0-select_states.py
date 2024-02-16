03d4c87dc811d3ee1b601dd91bc46bcf5978e282
nono <nour98nour@gmail.com>
2024-02-16 15:30:56 +0200

    betty for 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,14 @@ the database credentials as command-line
 arguments: the username, password, and database name.
 """
 if __name__ == "__main__":
-    import MySQLdb, sys
+    import MySQLdb
+    import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id;")
     rows = cur.fetchall()
