534490311eb6f52f20f7a12cd1d4ce2db8fde7df
nono <nour98nour@gmail.com>
2024-02-16 16:06:04 +0200

    2 task again

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name"
-                " = %s ORDER BY id;".format(sys.argv[4]))
+                " = '%s' ORDER BY id;".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
