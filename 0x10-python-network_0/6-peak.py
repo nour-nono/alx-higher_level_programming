f6f3fdab98a597caa6f76240a658758a0bb32dde
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-03-01 02:01:10 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,7 +17,7 @@ def find_peak(list_of_integers):
         if list_of_integers[0] > list_of_integers[1]:
             return list_of_integers[0]
         else:
-            return  list_of_integers[1]
+            return list_of_integers[1]
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\
             list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
