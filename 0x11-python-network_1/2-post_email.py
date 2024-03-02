48850006abeb37b134f6c3a0282aeddbcfe5e02f
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-03-02 03:30:39 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """takes in a URL and an email, sends a POST request """
-
 import sys
 import urllib.parse
 import urllib.request
@@ -8,12 +7,9 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
     values = {'email': email}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # data should be bytes
-
+    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
-
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
