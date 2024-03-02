dd0e972bbfccbd9fd8cf17d43d889b4215e3fe24
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-03-02 03:06:29 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""takes in a URL and an email, sends a POST request to the passed URL
+"""
+takes in a URL and an email, sends a POST request to the passed URL
 with the email as a parameter, and displays the body of the response
-(decoded in utf-8)"""
+(decoded in utf-8)
+"""
 
 from urllib import request, parse
 import sys
