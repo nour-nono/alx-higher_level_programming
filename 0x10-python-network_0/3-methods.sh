588494cc8e08da948954c9a369db51735463f6fa
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-03-23 03:01:36 +0200

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Display all HTTP methods the server of a given URL will accept.
 # curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
-curl -sIX OPTIONS 0.0.0.0:5000/route_4 | grep Allow | sed "s/Allow: //"
+curl -sIX OPTIONS "$1" | grep Allow | sed "s/Allow: //"
