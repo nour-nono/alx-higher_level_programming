ec09f5ef5e8adac7dca7c149fc4134ed8dafaec7
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-03-23 22:01:24 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -16,9 +16,8 @@ if __name__ == "__main__":
         "Accept": "application/vnd.github.v3+json",
         "X-GitHub-Api-Version": "2022-11-28"
     }
-    #basic = HTTPBasicAuth(username=sys.argv[1], password=sys.argv[2])
-    #r = requests.get(url, auth=basic)
-    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    basic = HTTPBasicAuth(username=sys.argv[1], password=sys.argv[2])
+    r = requests.get(url, auth=basic)
 
     res = r.json()
     user_id = res.get("id")
