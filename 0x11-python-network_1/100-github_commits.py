72a42d4be8e5020de169c7555c23d91aeb6b003d
nour-nono <nour98nour@gmail.com>
2024-03-24 03:10:13 +0200

    edit 100 file

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import requests
     import sys
     u = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[0]}/commits"
-    r = requests.get(url=u)
+    r = requests.get(u)
     for i in range(10):
 	    print(r.json()[i]['sha'], r.json()[i]['commit']['author']['name'])
