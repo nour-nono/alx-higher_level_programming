7d847e1c9a5bc1fbb67be083398390622c37a84a
nour-nono <nour98nour@gmail.com>
2024-03-24 03:27:55 +0200

    edit 100 file

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import requests
     import sys
-    u = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[0]}/commits"
+    u = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     r = requests.get(u)
     count = 0
     for i in r.json():
