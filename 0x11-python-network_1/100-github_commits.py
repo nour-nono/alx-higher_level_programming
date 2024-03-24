ef21ac5125beb7b5a5e7fc9e6bd8285a10d4221b
nour-nono <nour98nour@gmail.com>
2024-03-24 03:07:32 +0200

    edit 100 file

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Show the lastone ten commits in a repo github"""
-import requests
-from sys import argv
-
 if __name__ == "__main__":
     import requests
+    import sys
     u = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[0]}/commits"
     r = requests.get(url=u)
     for i in range(10):
