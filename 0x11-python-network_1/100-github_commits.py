7b25f8d672ead697423dc613c53326a0f7bc2538
nour-nono <nour98nour@gmail.com>
2024-03-24 03:06:09 +0200

    edit 100 file

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,15 +4,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    count = 0
-    page = "https://api.github.com/repos/{}/{}/commits".format(
-        argv[2], argv[1])
-
-    req = requests.get(page)
-    for commit in req.json():
-        sha = commit.get("sha")
-        author = commit.get("commit").get("author").get("name")
-        print("{}: {}".format(sha, author))
-        count += 1
-        if count > 9:
-            break
+    import requests
+    u = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[0]}/commits"
+    r = requests.get(url=u)
+    for i in range(10):
+	    print(r.json()[i]['sha'], r.json()[i]['commit']['author']['name'])
